ef799692678ae9cb7431d5b93af7e18621b15003
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2024-05-05 18:14:39 +0100

    Auth changes

--- util/auth.js
+++ util/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_KEY = 'AIzaSyAo_r7LsO_I9_RqiBgiDhYuu_XRW03NzGw'
+const API_KEY = ''
 
 async function authenticate(mode, email, password) {
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`
